8b55a6e5cc5f58f385f3d5ae16b65ac1cb2f1152
AyeshaAyubG <ayeshamirza5252@gmail.com>
2022-04-20 09:32:22 +0500

    implement upf side client

--- launcher.sh
+++ launcher.sh
@@ -10,11 +10,11 @@ trap _term SIGTERM
 env
 
 ./env_replace.sh
-./generate_certificate.sh
+#./generate_certificate.sh
 cat /etc/ipsec.conf
 cat /etc/ipsec.secrets
 
-./rules.sh
+#./rules.sh
 ./usr/sbin/ipsec start --nofork --debug-all
 ./usr/sbin/ipsec statusall
 
